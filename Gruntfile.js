fbf581b0608d34da0416af295033fcaec076bc13
Mike Holm <mholm815@gmail.com>
2014-11-13 22:10:08 -0700

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,59 @@
+/* global module:false */
+module.exports = function(grunt) {
+	var port = grunt.option('port') || 8000;
+	// Project configuration
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+
+		jshint: {
+			options: {
+				curly: false,
+				eqeqeq: true,
+				immed: true,
+				latedef: true,
+				newcap: true,
+				noarg: true,
+				sub: true,
+				undef: true,
+				eqnull: true,
+				browser: true,
+				expr: true,
+				globals: {
+					head: false,
+					module: false,
+					console: false,
+					unescape: false
+				}
+			},
+			files: [ 'Gruntfile.js' ]
+		},
+
+		connect: {
+			server: {
+				options: {
+					port: port,
+					base: '.'
+				}
+			}
+		},
+
+		watch: {
+			main: {
+				files: [ 'Gruntfile.js' ],
+				tasks: 'default'
+			}
+		}
+
+	});
+
+	// Dependencies
+	grunt.loadNpmTasks( 'grunt-contrib-jshint' );
+	grunt.loadNpmTasks( 'grunt-contrib-watch' );
+	grunt.loadNpmTasks( 'grunt-contrib-connect' );
+
+	// Default task
+	grunt.registerTask( 'default', [ 'jshint' ] );
+
+	// Serve locally
+	grunt.registerTask( 'serve', [ 'connect', 'watch' ] );
+};
